4984696349460cff343cac5f02612e079034e6ac
Devoopssss <158053383+keerthana1224@users.noreply.github.com>
2025-03-23 19:58:31 +0530

    Create SimpleStaticExample.java

---
+++ SimpleStaticExample.java
@@ -0,0 +1,12 @@
+public class SimpleStaticExample {
+
+    // Static method
+    public static void greet() {
+        System.out.println("Hello, welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        // Calling static method without creating an object
+        greet();
+    }
+}
